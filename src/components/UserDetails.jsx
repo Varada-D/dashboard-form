65abfdc0c55415c61021b1b555d0686475600338
Varada Deshpande <81087717+Varada-D@users.noreply.github.com>
2023-11-25 22:55:32 +0530

    Update UserDetails.jsx

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,16 +1,21 @@
 const UserDetails = () => {
 	return (
 		<>
-			<div className="text-2xl font-bold">UserDetails</div>
-			<label htmlFor="Name">Name</label> <br/>
-			<input type="text" name="name" id="name" className="border border-black"/>
-			<br/><br/>
-			<label htmlFor="contect">Contact Number</label> <br/>
-			<input type="number" name="contact" id="contact" className="border border-black"/>
-			<br/><br/>
-			<label htmlFor="email ">Email ID</label> <br/>
-			<input type="email" name="email" id="email" className="border border-black"/>
-			<br/><br/>
+			<h2 className="text-2xl font-bold">UserDetails</h2>
+			<div className="flex">
+				<div class="w-48 md:w-72 sm:w-96">
+					<label htmlFor="Name" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
+					<input type="text" name="name" id="name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2"/>
+				</div>
+				<div class="w-48 md:w-72 sm:w-96">
+					<label htmlFor="contect">Contact Number</label>
+					<input type="number" name="contact" id="contact" className="border border-black"/>
+				</div>
+				<div class="w-48 md:w-72 sm:w-96">
+					<label htmlFor="email ">Email ID</label>
+					<input type="email" name="email" id="email" className="border border-black"/>
+				</div>
+			</div>
 			<label htmlFor="role">Role</label> <br/>
 			Student <input type="checkbox" name="role" id="chx:student" value="Student"/>
 			Faculty <input type="checkbox" name="role" id="chx:faculty" value="Faculty"/>
